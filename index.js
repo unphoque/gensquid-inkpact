be0be72791a840e2991bb3e4d296d1a59e06d9fb
senetale <alex.senet38@gmail.com>
2022-08-12 13:43:25 +0200

    Ready to go ?

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const fs = require('fs')
 
 
 const commands = [];
-const commandFiles = fs.readdirSync('./commands/').filter(file => file.endsWith('.js'));
+const commandFiles = fs.readdirSync(__dirname+'/commands/').filter(file => file.endsWith('.js'));
 
 for (const file of commandFiles) {
     const command = require(`./commands/${file}`);
