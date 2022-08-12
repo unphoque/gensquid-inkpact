1a66fa1a7eb241bb24ca7d5ed7c29b534195f823
senetale <alex.senet38@gmail.com>
2022-08-12 14:50:12 +0200

    ä

--- commands/util.js
+++ commands/util.js
@@ -2,7 +2,7 @@ const toFileString=function(txt){
     txt=txt.replaceAll(" ","_")
     txt=txt.replaceAll("(","").replaceAll(")","")
 
-    txt=txt.replaceAll("à","a")
+    txt=txt.replaceAll("à","a").replaceAll("ä","a")
     txt=txt.replaceAll("ç","c")
     txt=txt.replaceAll("é","e").replaceAll("è","e")
     txt=txt.replaceAll("ï","i")
