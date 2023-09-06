08112d1a0d88929e243a4138c1bf009ceb83ed23
senetal <alex.senet38@gmail.com>
2023-09-06 13:56:14 +0200

    Ajout de l'accent circonflexe

--- commands/util.js
+++ commands/util.js
@@ -4,10 +4,10 @@ const toFileString=function(txt){
     txt=txt.replaceAll("&","_")
     txt=txt.replaceAll("?","_")
 
-    txt=txt.replaceAll("à","a").replaceAll("ä","a")
+    txt=txt.replaceAll("à","a").replaceAll("ä","a").replaceAll("â","a")
     txt=txt.replaceAll("ç","c")
-    txt=txt.replaceAll("é","e").replaceAll("è","e")
-    txt=txt.replaceAll("ï","i")
+    txt=txt.replaceAll("é","e").replaceAll("è","e").replaceAll("ê","e")
+    txt=txt.replaceAll("ï","i").replaceAll("î","i")
 
     return txt
 }
