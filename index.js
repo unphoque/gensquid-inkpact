ebf1abd76535b6f2891b1b21fdb84a1537e7388e
un_phoque <unphoque38@gmail.com>
2024-12-14 00:13:49 +0100

    Bientôt

--- index.js
+++ index.js
@@ -185,6 +185,7 @@ client.on("messageCreate", async message => {
         let st=1830294000000
         let end=2114377200000
         let date = new Date(st + Math.random() * (end - st))
+        date.setUTCFullYear(2028+Math.random()*(2037-2028))
         let embed=new MessageEmbed().setDescription(`<t:${date.getTime()}>`)
         message.reply({embeds: [embed]})
     }
