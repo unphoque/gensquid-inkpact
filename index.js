3484b2435de8f59ba8079c3f14dd771c1dc3f073
un_phoque <unphoque38@gmail.com>
2026-03-31 20:15:41 +0200

    Poisson d'avril

--- index.js
+++ index.js
@@ -76,8 +76,8 @@ const proba=require("./commands/proba")
 const blackmarket=require("./commands/blackmarket")
 const achievement=require("./commands/achievement")
 
-const dateMaintenance=Date.UTC(2025,11,25,4,0,0,0)
-const msgMaintenance="# HO HO HO !"
+const dateMaintenance=Date.UTC(2026,3,1,0,0,0,0)
+const msgMaintenance="I'm cooking."
 
 client.on('ready', async () => {
     console.log(`Logged in as ${client.user.tag}!`);
